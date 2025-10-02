fa6af4d00776dd842a4396304a20c686d83e6a98
electrohyun <dev.electrohyun@gmail.com>
2025-10-02 13:09:49 +0900

    feat: (#9) 회원가입 mock 데이터 추가

--- src/mocks/auth.mock.ts
+++ src/mocks/auth.mock.ts
@@ -15,8 +15,6 @@ const USERS = [
   },
 ];
 
-const ERROR_USERS = [{ username: 'hacker1234', password: 'asdf1234!' }];
-
 export const authHandlers = [
   http.post('auth/login', async ({ request }) => {
     const { username, password } = (await request.json()) as {
@@ -24,10 +22,10 @@ export const authHandlers = [
       password: string;
     };
 
-    if (ERROR_USERS.some((u) => u.username === username)) {
+    if (username === 'hackeruser1') {
       return HttpResponse.json(
         {
-          code: 'INTERNAL_SERVER_ERROR',
+          code: '500',
           message: '서버에 알 수 없는 오류가 발생했습니다.',
           errors: [],
         },
@@ -58,6 +56,64 @@ export const authHandlers = [
           id: 'user-1',
           username: user.username,
           nickname: user.nickname,
+        },
+      },
+      { status: 200 },
+    );
+  }),
+  http.post('auth/signup', async ({ request }) => {
+    const { username, nickname, password } = (await request.json()) as {
+      username: string;
+      nickname: string;
+      password: string;
+    };
+
+    if (username === 'erroruser1') {
+      return HttpResponse.json(
+        {
+          code: 500,
+          message: '서버 에러 발생',
+          errors: [],
+        },
+        { status: 500 },
+      );
+    }
+
+    const errors: { field: string; reason: string }[] = [];
+
+    if (username === 'alreadyuser1') {
+      errors.push({
+        field: 'username',
+        reason: '이미 사용중인 아이디입니다.',
+      });
+    }
+
+    if (nickname === 'alreadyuser2') {
+      errors.push({
+        field: 'nickname',
+        reason: '이미 사용중인 닉네임입니다.',
+      });
+    }
+
+    if (errors.length > 0) {
+      return HttpResponse.json(
+        {
+          code: 409,
+          message: '중복 검사 실패',
+          errors,
+        },
+        { status: 409 },
+      );
+    }
+
+    return HttpResponse.json(
+      {
+        accessToken: 'mock-access-token-12345',
+        refreshToken: 'mock-refresh-token-67890',
+        user: {
+          id: 'user-1',
+          username,
+          nickname,
         },
       },
       { status: 200 },
