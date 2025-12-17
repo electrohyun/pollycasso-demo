81eeef281efb5647a8561445901846d700f30758
electrohyun <dev.electrohyun@gmail.com>
2025-12-17 19:59:04 +0900

    feat: (#46) PlayerAvatar 및 ItemIcon UI 컴포넌트 구현

---
+++ src/entities/game/ui/PlayerAvatar.tsx
@@ -0,0 +1,24 @@
+const getLevelColor = (level: number) => {
+  if (level >= 20) return 'bg-red-500';
+  if (level >= 10) return 'bg-yellow-400';
+  return 'bg-green-500';
+};
+
+export const PlayerAvatar = ({
+  nickname,
+  level,
+}: {
+  nickname: string;
+  level: number;
+}) => (
+  <div className="flex flex-col items-center">
+    <div className="relative w-[70px] h-[70px] bg-white rounded-full mx-auto shadow-md">
+      <div
+        className={`absolute -top-2 -left-2 w-8 h-8 rounded-full border-2 border-[#003D00] ${getLevelColor(level)}`}
+      />
+    </div>
+    <p className="text-center mt-2 text-white font-bold truncate w-20">
+      {nickname}
+    </p>
+  </div>
+);
