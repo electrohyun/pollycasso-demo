7d7bc03670a003278b3b10c4c5b57f861a7054ee
electrohyun <dev.electrohyun@gmail.com>
2025-12-22 19:17:28 +0900

    feat: (#54) mock 데이터 연결을 위한 useGameDrawing 훅 작성

---
+++ src/widgets/game-drawing/model/useGameDrawing.ts
@@ -0,0 +1,31 @@
+import { useAuthStore } from '@/features/auth/model'; // TODO: useAuthStore 위치변경(shared)
+import { MOCK_GAME_DRAWING } from '@/mocks/game.mock';
+import type { DrawingContext } from '@/entities/game/model/types';
+import { useMemo } from 'react';
+
+export const useGameDrawing = () => {
+  const user = useAuthStore((state) => state.user);
+
+  const gameState = MOCK_GAME_DRAWING;
+  const { status, players, endsAt, phaseContext } = gameState;
+
+  const myData = useMemo(() => {
+    if (!user) return null;
+    return players.find((p) => p.userId === user.id);
+  }, [players, user]);
+
+  const myInventory = myData?.inventory || [];
+
+  const currentTheme = useMemo(() => {
+    const context = phaseContext as DrawingContext;
+    return context?.currentTheme || '주제 대기 중...';
+  }, [phaseContext]);
+
+  return {
+    status,
+    players,
+    endsAt,
+    myInventory,
+    currentTheme,
+  };
+};
--- src/widgets/game-drawing/ui/index.tsx
+++ src/widgets/game-drawing/ui/index.tsx
@@ -1,22 +1,22 @@
-import { GAME_DATA } from '@/mocks/game.mock';
 import { PlayerSidebar } from '@/features/game/ui/PlayerSidebar';
 import { InventoryPanel } from '@/features/game/ui/InventoryPanel';
 import { GameHeader } from '@/features/game/ui/GameHeader';
 import { GameCanvas } from '@/features/game/ui/GameCanvas';
+import { useGameDrawing } from '@/widgets/game-drawing/model/useGameDrawing';
 
 const GameWidget = () => {
-  const { players, myItems, timer, currentTheme } = GAME_DATA;
+  const { players, myInventory, endsAt, currentTheme } = useGameDrawing();
 
   return (
     <div className="w-full h-screen flex justify-between items-center font-ssrm px-20 py-4 overflow-hidden gap-16">
       <PlayerSidebar players={players} />
 
       <main className="w-full h-full rounded-3xl bg-white shadow-xl flex flex-col relative overflow-hidden">
-        <GameHeader theme={currentTheme} timer={timer} />
+        <GameHeader theme={currentTheme} timer={endsAt} />
         <GameCanvas />
       </main>
 
-      <InventoryPanel myItems={myItems} />
+      <InventoryPanel myItems={myInventory} />
     </div>
   );
 };
