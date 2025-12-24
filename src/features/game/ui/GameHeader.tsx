930c497fae6c722e258a4d81341b6e8a53468e9c
electrohyun <dev.electrohyun@gmail.com>
2025-12-24 17:50:47 +0900

    publish: (#58) marquee 안내 텍스트 크기 증가

--- src/features/game/ui/GameHeader.tsx
+++ src/features/game/ui/GameHeader.tsx
@@ -31,7 +31,7 @@ export const GameHeader = ({ currentTheme, endsAt }: GameHeaderProps) => {
     <>
       <div className="mt-4 py-2 text-black bg-[#D9D9D9]/45 shadow-sm shadow-black/40">
         <Marquee gradient={false} speed={50}>
-          <span className="mx-4 font-bold">{UI_TEXT.NOTICE}</span>
+          <span className="text-lg mx-4 font-bold">{UI_TEXT.NOTICE}</span>
         </Marquee>
       </div>
 
