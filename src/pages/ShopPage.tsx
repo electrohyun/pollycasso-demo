121b19610118fa5649f2d81a53ac805628790355
electrohyun <dev.electrohyun@gmail.com>
2026-01-26 20:57:59 +0900

    feat: (#115) 상점 아이템 정렬 기능 추가

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,12 +1,8 @@
+import { useMemo } from 'react';
 import { useCart } from '@/features/cart';
-import {
-  SHOP_CATEGORIES,
-  useShopFilter,
-  type CategoryType,
-} from '@/features/shop';
+import { useShopFilter, SHOP_CATEGORIES } from '@/features/shop';
 import { MOCK_PRODUCTS } from '@/mocks/shop.mock';
 import { ShopSidebar, ShopProductList, ShopProfilePanel } from '@/widgets/shop';
-import { useMemo } from 'react';
 
 const ShopPage = () => {
   const { cart, addToCart, removeFromCart } = useCart();
@@ -21,14 +17,25 @@ const ShopPage = () => {
     handleCategoryChange,
   } = useShopFilter();
 
-  const filteredProducts = useMemo(() => {
-    const targetCategoryLabel = SHOP_CATEGORIES[activeCategory as CategoryType];
+  const processedProducts = useMemo(() => {
+    const targetCategoryLabel = SHOP_CATEGORIES[activeCategory];
 
-    // 2. 변환된 한글 라벨로 목데이터 필터링
-    return MOCK_PRODUCTS.filter(
+    const filtered = MOCK_PRODUCTS.filter(
       (item) => item.subCategory === targetCategoryLabel,
     );
-  }, [activeCategory]);
+
+    switch (activeSort) {
+      case 'COST':
+        return filtered.sort((a, b) => a.price - b.price);
+
+      case 'LEVEL':
+        return filtered.sort((a, b) => a.level - b.level);
+
+      case 'POPULAR':
+      default:
+        return filtered;
+    }
+  }, [activeCategory, activeSort]);
 
   return (
     <div className="flex items-center justify-center w-full min-h-screen gap-[24px] font-ssrm font-bold">
@@ -43,7 +50,7 @@ const ShopPage = () => {
       />
 
       <ShopProductList
-        products={filteredProducts}
+        products={processedProducts}
         cart={cart}
         onAddToCart={addToCart}
       />
