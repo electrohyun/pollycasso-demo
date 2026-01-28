4faed8d00b222a860c109e010627089c39518094
electrohyun <dev.electrohyun@gmail.com>
2026-01-28 16:50:24 +0900

    feat: (#36) 임시 플레이어 및 옷장 상태 스토어 작성

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -1,2 +1,4 @@
 export * from './model/types';
 export { useAuthStore } from './model/useAuthStore';
+export { usePlayerStore } from './model/usePlayerStore';
+export { useWardrobeStore } from './model/useWardrobeStore';
