7ec6d9218d461a4704a2bb1fe0c2639aa72a85e6
electrohyun <dev.electrohyun@gmail.com>
2026-05-26 21:12:27 +0900

    --amend

--- src/app/PrivateRoute.tsx
+++ src/app/PrivateRoute.tsx
@@ -1,6 +1,15 @@
-import { Outlet } from 'react-router';
+import { Navigate, Outlet, useLocation } from 'react-router';
+
+import { useAuthStore } from '@/entities/user';
 
 const PrivateRoute = () => {
+  const location = useLocation();
+  const user = useAuthStore((state) => state.user);
+
+  if (!user) {
+    return <Navigate to="/login" replace state={{ from: location }} />;
+  }
+
   return <Outlet />;
 };
 
